5006c3b4876a5fd7a3b9ba109d506d0499b2ce78
Sammenspiel <skriever@gmail.com>
2013-10-26 10:44:14 -0400

    Stubbed out tests by Wendy for week 2

--- src/tests/Week2Tests.java
+++ src/tests/Week2Tests.java
@@ -8,10 +8,116 @@ import org.junit.Test;
 import code.Conquest;
 
 public class Week2Tests {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	
+	@Before //things to be done before every test
+	public void setup() {
+		
 	}
 
+	@Test //test for adding only 2 players
+	public void test1() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test for adding 3 players
+	public void test2() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test for adding 20 players
+	public void test3() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test for adding 21 players
+	//EXCEPTION EXPECTED
+	public void test4() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test for giving two players in a row the same names
+	public void test5() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test for giving two nonconsecutive players the same name
+	public void test6() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test for correct number of schools in week two: 6
+	public void test7() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test for correct number of depts in week two: 24
+	public void test8() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //text access to dept names in sciences school
+	public void test9() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test access to dept names in humanities school
+	public void test10() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test access to dept names in arts school
+	public void test11() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test access to dept names in engineering school
+	public void test12() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test access to dept names in law school
+	public void test13() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test access to dept names in medicine school
+	public void test14() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test adjacencies of one dept in sciences
+	public void test15() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test adjacencies of one dept in humanities
+	public void test16() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test adjacencies of one dept in arts
+	public void test17() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test adjacencies of one dept in engineering
+	public void test18() {
+		fail("Not yet implemented");
+	}
+	
+	@Test //test adjacencies of one dept in law
+	public void test19() {
+		fail("Not yet implemented");
+	}
+	
+	@Test ////test adjacencies of one dept in medicine
+	public void test20() {
+		fail("Not yet implemented");
+	}
+	
+	@After
+	public void teardown(){
+		
+	}
+	
 }
